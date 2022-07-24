959b50efb8aed1c912647584996f61a7ba824667
Phil Branch <philbranch243@gmail.com>
2022-07-24 13:24:00 -0500

    accidentally tracked node modules

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const exp = require("constants");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 //NOW ADD MIDDLEWARE
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'A1-Branch3idinI',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
 
 
 
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //END OF FILE! THE FOLLOWING STATEMENTS MUST BE LAST
 // Default response for any other request (Not Found)
